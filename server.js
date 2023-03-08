c2233d04b14e3db119d71b83930f6fff6afa719a
Thiên Vũ Đặng <vudt20411c@st.uel.edu.vn>
2023-03-08 11:31:10 +0700

    latest

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var express = require('express');
+var express = require("express");
 var app = express();
 app.use(express.static(__dirname));
-app.listen('3300');
-console.log('Running at\nhttp://localhost:3300');
+app.listen("3300");
+console.log("Running at\nhttp://localhost:3300");
